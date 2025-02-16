8e1e1cd07e1d5d245e148314c3afd0084f0e0a43
artisan73 <magemberonald73@gmail.com>
2025-02-16 10:32:45 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,18 @@
 const mobileMenuButton = document.getElementById('mobile-menu-button');
-        const mobileMenu = document.getElementById('mobile-menu');
-
-        mobileMenuButton.addEventListener('click', () => {
-            const isOpen = mobileMenu.classList.toggle('hidden');
-            mobileMenuButton.innerHTML = isOpen ? '<span class="text-3xl">=</span>' : '<span class="text-3xl">×</span>';
-        });
-
-        document.addEventListener('click', (event) => {
-            if (!mobileMenu.contains(event.target) && !mobileMenuButton.contains(event.target)) {
+const mobileMenu = document.getElementById('mobile-menu');
+mobileMenuButton.addEventListener('click', () => {
+        const isOpen = mobileMenu.classList.toggle('hidden');
+        mobileMenuButton.innerHTML = isOpen ? '<span class="text-3xl">=</span>' : '<span class="text-3xl">×</span>';
+});
+document.addEventListener('click', (event) => {
+        if (!mobileMenu.contains(event.target) && !mobileMenuButton.contains(event.target)) {
                 mobileMenu.classList.add('hidden');
                 mobileMenuButton.innerHTML = '<span class="text-3xl">=</span>';
-            }
-        });
-
-        mobileMenu.querySelectorAll('a').forEach(link => {
-            link.addEventListener('click', () => {
+        }
+});
+mobileMenu.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
                 mobileMenu.classList.add('hidden');
                 mobileMenuButton.innerHTML = '<span class="text-3xl">=</span>';
-            });
         });
+});
